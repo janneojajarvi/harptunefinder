f42dde08d89ff646dc078d9b7bfccccc48c66718
janneojajarvi <janne.ojajarvi@gmail.com>
2026-04-29 10:11:02 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,42 @@
-const CACHE_NAME = 'harptune-v9';
+const CACHE_NAME = 'harptune-v10';
 const ASSETS = [
   './',
   './index.html',
   './style.css',
   './script.js',
   './manifest.json',
-  './harptunefinderkuva.png'
+  './favicon.ico',
+  './kuva.png',
+  './harptunefinderkuva.png',
+  
+  // Nuottikirjastot
+  './FinnishTunes2b.js',
+  './extrasetti5.js',
+  
+  // Folkwiki-kokoelmat
+  './folkwikiSet1.js',
+  './folkwikiSet2.js',
+  './folkwikiSet3.js',
+  
+  // SessionSet-sarja (01-18)
+  './sessionSet01.js',
+  './sessionSet02.js',
+  './sessionSet03.js',
+  './sessionSet04.js',
+  './sessionSet05.js',
+  './sessionSet06.js',
+  './sessionSet07.js',
+  './sessionSet08.js',
+  './sessionSet09.js',
+  './sessionSet10.js',
+  './sessionSet11.js',
+  './sessionSet12.js',
+  './sessionSet13.js',
+  './sessionSet14.js',
+  './sessionSet15.js',
+  './sessionSet16.js',
+  './sessionSet17.js',
+  './sessionSet18.js'
 ];
 
 // Asennusvaihe: Tallennetaan perustiedostot välimuistiin
