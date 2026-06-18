793c87e2325a60129b6ce4c28509b16f943dedd2
janneojajarvi <janne.ojajarvi@gmail.com>
2026-06-19 00:15:15 +0300

    Add files via upload

---
+++ blues.js
@@ -0,0 +1,34 @@
+var blues = [
+  {
+    name: "Saint Louis Blues",
+    abc: "X:1\nT:Saint Louis Blues\n% last edit 03-02-2013\nC:W.C.Handy\nM:2/4\nL:1/16\nQ:1/4=72\nK:G\n  z2|{^A}BdBG- G4 |z ^DEG _B A3|G8-  |G6 \\n  z2|    zEGA- A4 |z  EGA _B A3|G8-  |G6\n  z2|    FAFD  D4 |z2 _BB  B A3|G8-  |G6 \\n  z2|{^A}BdBG- GG3|z ^DEG _B A3|G8-  |G6\n  z2|    zEGA- AA3|z ^DEG _B A3|G8-  |G6 \\n  z2|    FAFD- DD3|z2 _BB  B A3|G8-  |G4||\n%\nzddd|    dd3-  d4 |z  c2^c d_B3|A8-  |A4- A  \\n c2c|    cc3-  c4 |z2 c^c  d A3|G8-  |G4  z2\n  dd|    dd3-  d4 |z  c2^c d_B3|A8-  |A4  z2 \\n  c2|    cc2c- c4 |z2 c^c  d A3|G4 A4|d6||\n%\n(^AB)| G2(^AB) G2(AB) |G2(^AB) (GA)B  G-|G8-|G4 z2\n(^AB)|=A2  GE  G2(A_B)|A2(GE)   G(^AB)G-|G8-|G4 z2\n(^AB)| d2(^AB) D(AB)C-|C(^AB)D- D (AB)G-|G8 |z4 z2|]"
+  },
+  {
+    name: "St Louis Blues",
+    abc: "X:1\nT:St Louis Blues\n% last edit 03-02-2013\nC:W.C.Handy\nM:2/4\nL:1/16\nQ:1/4=72\nK:G\n  z2|{^A}BdBG- G4 |z ^DEG _B A3|G8-  |G6 \\n  z2|    zEGA- A4 |z  EGA _B A3|G8-  |G6\n  z2|    FAFD  D4 |z2 _BB  B A3|G8-  |G6 \\n  z2|{^A}BdBG- GG3|z ^DEG _B A3|G8-  |G6\n  z2|    zEGA- AA3|z ^DEG _B A3|G8-  |G6 \\n  z2|    FAFD- DD3|z2 _BB  B A3|G8-  |G4||\n%\nzddd|    dd3-  d4 |z  c2^c d_B3|A8-  |A4- A  \\n c2c|    cc3-  c4 |z2 c^c  d A3|G8-  |G4  z2\n  dd|    dd3-  d4 |z  c2^c d_B3|A8-  |A4  z2 \\n  c2|    cc2c- c4 |z2 c^c  d A3|G4 A4|d6||\n%\n(^AB)| G2(^AB) G2(AB) |G2(^AB) (GA)B  G-|G8-|G4 z2\n(^AB)|=A2  GE  G2(A_B)|A2(GE)   G(^AB)G-|G8-|G4 z2\n(^AB)| d2(^AB) D(AB)C-|C(^AB)D- D (AB)G-|G8 |z4 z2|]"
+  },
+  {
+    name: "St James Infirmary",
+    abc: "X:1\nT:St James Infirmary\nL:1/8\nM:4/4\nI:linebreak $\nK:C\nV:1 treble nm=\nV:1\n z2 z2 z2 DF |: A3 F G3 A | FD- D6 | %3\n A3 A d2 c2 | A6 DF |$ %5\n A3 A/F/ G3 A | FD- D4 z D | %7\n F3 D A2 FE | D8 :| %9"
+  },
+  {
+    name: "Frankie and Albert",
+    abc: "X:1\nT:Frankie and Albert\nM:4/4\nL:1/4\nK:F\n F3/2 G/2A/2 G/2 A| A/2 F/2- F2 z| A3/2 G/2A/2 A F/2-| F2 z2| B/2 B B/2 d d|\\n d/2 B3/2 z d/2d/2| d d c B| A F F F| G2 z2| z A3/4 G/4 A3/4- F/4 G|\\n F2 z2|"
+  },
+  {
+    name: "The House of the Rising Sun",
+    abc: "X:1\nT:The House of the Rising Sun\nC:Traditional\nM:3/4\nL:1/4\nK:F\nA | D2 (E/2D/2) | F  (G/2^G/2) | A (F/2E/2) |\nD2 A | d2 A | c (A/2G/2) | A3- |\nA2 ^c/2d/2 | d- d D | F/2 F3/2 A/2 | A/2\nA/2 A F/2E/2 | D2 G | A2 A |\nA2 (F/2E/2) | D2- D- | D z z | z z2 | z3 |\nz z =B ||\nK:G\nE2 F/2E/2 | G/2 G3/2 A/2^A/2 | B2 G/2A/2 ||\nE2 B | e2 B | d2 B/2A/2 | B3- | B2 ^d/2e/2 |\ne- e E | g G B | B/2B/2 B G/2F/2 |\nE2  A | B2 B | B2 G/2F/2 | E2- E- | E2 A\n |\nB2 B/2B/2 | B2 G/2F/2 | E2- E- | E2 z :|"
+  },
+  {
+    name: "Careless Love",
+    abc: "X:1\nT:Careless Love\nM:C|\nL:1/4\nQ:1/2=80\nK:F\nA3 F|E C E G|F4-  |F4||\\nA3 B|c c d c|G4-  |G4||\nA3 B|c3   ^c|d2 B2|F4||\\nA3 F|E C E G|F4-  |F4|]"
+  },
+  {
+    name: "Just A Closer Walk With Thee",
+    abc: "X:1\nT:Just A Closer Walk With Thee\nM:4/4\nL:1/8\nK:C\n G3 ^G A/2 E3/2 =G^F| =F8| d3 d B/2 G3/2 AB| A/2- G3/2- G6| e3 g e/2 c3/2 dc|\\n c6 cA| G3 G G2 G/2 G3/2| G8|"
+  },
+  {
+    name: "Georgia on my Mind",
+    abc: "X:1\nT:Georgia on my Mind\nL:1/4\nM:4/4\nK:F\nA c3|A G3|z A d A|G3F/G/|\nAc ed|Bd AG|c2-c2|d/d/d/d/ ^G2|\nA c3|A G3|z A d A|G3F/G/|\nAc ed|BD AA|F2-F2-|FF z2|\nDF GA|FD F2|DF GA|cA =B2|\nDF GA|cde>d|cA cA|A2 ^G2|\nA c3|A G3|z A d A|G3F/G/|\nAc ed|BD AA|F4-|F2 z2|]"
+  }
+];
